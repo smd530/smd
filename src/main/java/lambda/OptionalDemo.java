010b8d694af2e62b7e999f2f20446cc863315ccc
shanmingda <git@github.com:smd530>
2023-04-29 21:41:02 +0800

    optional

--- src/main/java/lambda/OptionalDemo.java
+++ src/main/java/lambda/OptionalDemo.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class OptionalDemo {
 
@@ -24,16 +25,26 @@ public class OptionalDemo {
     }
 
 
+
     /**
      * Optional.ofNullable(T); 此方法无论传入的对象是否为空 都不会空指针
      */
     public static void main(String[] args) {
-        Author author = getAuthor();
-        Optional<Author> author1 = Optional.ofNullable(author);
-        author1.ifPresent(author22 -> System.out.println(author22.getName()));
-        Optional<Author> authorOptional = getAuthorOptional();
-        authorOptional.ifPresent(author2 -> System.out.println(author2.getName()));
-        Optional<Author> of = getOf();
+//        Author author = getAuthor();
+//        Optional<Author> author1 = Optional.ofNullable(author);
+//        author1.ifPresent(author22 -> System.out.println(author22.getName()));
+//        Optional<Author> authorOptional = getAuthorOptional();
+//        authorOptional.ifPresent(author2 -> System.out.println(author2.getName()));
+//        Optional<Author> of = getOf();
+//
+        Optional<Author> authorOptional1 = getAuthorOptional();
+        Author author2 = authorOptional1.orElseGet(new Supplier<Author>() {
+            @Override
+            public Author get() {
+                return new Author(2L, "神奇女侠", 5000, "女神", Lists.newArrayList());
+            }
+        });
+        System.out.println(author2.toString());
 
 
     }
